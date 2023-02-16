329c4c90218d340f304de59557d368d5d1fcaa0f
nitish choudhary <nitish@webkorps.com>
2023-02-16 23:59:56 +0530

    only test for api

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,23 @@
+class BooksController < ApplicationController
+ 
+  def index
+    render json: Book.all
+  
+  end
+   
+  def create
+     book=Book.new(book_params)
+     if book.save
+      render json: book, status: :created 
+     else
+      render json: book.erors, status: :unprocessable_entity
+     end
+     
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:author)
+  end
+end
